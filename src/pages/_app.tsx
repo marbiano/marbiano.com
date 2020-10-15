e689a78a8db38e955d049d55ee8aff1fe99f3e51
Martin Bavio <mbavio@gmail.com>
2020-10-14 23:24:19 -0300

    Add starter repo

---
+++ src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import { AppProps } from 'next/app';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+const IndexPage: React.FC = () => {
+  return <div>Hi</div>;
+};
+
+export default IndexPage;
