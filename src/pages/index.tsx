c86a7f54d343b8c05fcc0400ca0c711ce9a7d05c
Martin Bavio <mbavio@gmail.com>
2020-10-15 01:04:45 -0300

    Remove noJS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,19 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { gsap } from 'gsap';
 import { Draggable } from 'gsap/dist/Draggable';
 import Logo from '@/components/Logo';
 
 const IndexPage: React.FC = () => {
-  const logoRef = useRef();
-
   useEffect(() => {
     gsap.registerPlugin(Draggable);
-    Draggable.create('#el-1', {
-      zIndexBoost: false,
-    });
-    Draggable.create('#el-2', {
-      zIndexBoost: false,
-    });
-    Draggable.create('#el-3', {
-      zIndexBoost: false,
-    });
+    Draggable.create('#el-1');
+    Draggable.create('#el-2');
+    Draggable.create('#el-3');
   }, []);
 
   return (
     <div className="container">
       <Logo />
-      {/* <img
-        src="/logo1.svg"
-        alt="Marbiano logo"
-        className="logo"
-        ref={logoRef.current}
-        id="logo"
-      /> */}
       <h1>
         <span className="name">Martin Bavio</span>
         <span className="role">, web developer.</span>
@@ -36,7 +21,5 @@ const IndexPage: React.FC = () => {
     </div>
   );
 };
-
-export const config = { unstable_runtimeJS: false };
 
 export default IndexPage;
