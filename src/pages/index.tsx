5eb636376bbed8ba10157f26897f02517908ee90
Martin Bavio <mbavio@gmail.com>
2020-10-15 23:43:58 -0300

    Add proper SEO content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,34 @@
 import { useState } from 'react';
 import { EyeClosedIcon, EyeOpenIcon } from '@modulz/radix-icons';
 import Canvas from '@/components/Canvas';
+import SiteHead from '@/components/SiteHead';
 
 const IndexPage: React.FC = () => {
   const [artMode, setArtMode] = useState(false);
   return (
-    <div className="container">
-      <Canvas artMode={artMode} />
-      <h1>
-        {artMode ? (
-          <span className="tagline">what can you do with less?</span>
-        ) : (
-          <>
-            <span className="name">Martin Bavio</span>
-            <span className="role">, web developer.</span>
-          </>
-        )}
-      </h1>
-      <button
-        type="button"
-        onClick={() => setArtMode(!artMode)}
-        className="cta"
-      >
-        {artMode ? <EyeOpenIcon /> : <EyeClosedIcon />}
-      </button>
-    </div>
+    <>
+      <SiteHead title="Martin Bavio, web developer." />
+      <div className="container">
+        <Canvas artMode={artMode} />
+        <h1>
+          {artMode ? (
+            <span className="tagline">what can you do with less?</span>
+          ) : (
+            <>
+              <span className="name">Martin Bavio</span>
+              <span className="role">, web developer.</span>
+            </>
+          )}
+        </h1>
+        <button
+          type="button"
+          onClick={() => setArtMode(!artMode)}
+          className="cta"
+        >
+          {artMode ? <EyeOpenIcon /> : <EyeClosedIcon />}
+        </button>
+      </div>
+    </>
   );
 };
 
